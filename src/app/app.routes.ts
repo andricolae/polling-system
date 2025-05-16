55a6fba73edd9c06a6eaba0216db1ca31e227693
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-05-16 10:31:23 +0300

    add coming-soon component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { ComingSoonComponent } from './coming-soon/coming-soon.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: ComingSoonComponent
+    },
+    {
+        path: 'login', component: AuthComponent
+    }
+];
--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import { environment } from './environment';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import{provideAuth, getAuth} from '@angular/fire/auth';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent,{
   providers:[
     provideFirebaseApp(()=>initializeApp(environment.firebaseConfig)),
     provideAuth(()=>getAuth()),
     provideFirestore(()=>getFirestore()),
+    provideRouter(routes),
   ]
 
 })
