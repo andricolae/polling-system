2ca3ac2c502b83235fc1c3431ff2ab46662d4c48
Marita-Thuri Anna <maritathurianna@gmail.com>
2025-05-16 22:09:44 +0300

    created: homepage, header, add poll page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { PollCreateComponent } from './pages/poll-create/poll-create.component';
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'create', component: PollCreateComponent }
+];
