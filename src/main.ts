eb4b95d652162cdaabdbe9db8a31f4db17a3f56a
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-05-16 10:08:17 +0300

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { environment } from './environment';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import{provideAuth, getAuth} from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 bootstrapApplication(AppComponent,{
   providers:[
-    //provideFirebaseApp(()=>initializeApp(environment.firebaseConfig)),
+    provideFirebaseApp(()=>initializeApp(environment.firebaseConfig)),
+    provideAuth(()=>getAuth()),
+    provideFirestore(()=>getFirestore()),
   ]
 
 })
