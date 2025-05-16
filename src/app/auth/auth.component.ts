6211d1977f488a7af772508c75a8d9fcfe0d765d
Tudor-debug819 <tovidiua@emeal.nttdata.com>
2025-05-16 10:39:07 +0300

    add form for login

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,11 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import{FormsModule, NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './auth.component.html',
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+
+  email = '';
+  password = '';
+
+  login(){
+    
+  }
 
 }
